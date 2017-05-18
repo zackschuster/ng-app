bf9dd610d437320a4c9714b502e2414842041321
Zack Schuster <email@zackschuster.com>
2017-05-18 16:34:08 -0700

    import router in core

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import * as angular from 'angular';
+import 'angular-ui-router';
 
 import { DataService } from 'app/core/data.service';
 import { Logger } from 'app/core/logger';
 
 export default
 	angular
-		.module('core', [])
+		.module('core', ['ui.router'])
 		.service('dataService', DataService)
 		.service('logger', Logger);
