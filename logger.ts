cb5c14053cbacc47d8fa67fcc596d1fc3599ee42
Zack Schuster <email@zackschuster.com>
2017-05-18 15:14:44 -0700

    logger works

--- logger.ts
+++ logger.ts
@@ -1,47 +1,38 @@
-import Noty from 'noty';
+import Noty, { NotyType } from 'noty';
 import { ILogCall, ILogService } from 'angular';
 import { config } from 'app/core/config';
+import { Callback, Indexed } from '@ledge/types';
 
 export class Logger {
 	public log: ILogCall;
 	private noty: Noty;
+	private typeMap: Indexed<string> = {
+		warning: 'warn',
+		success: 'log',
+		info: 'info',
+		error: 'error',
+	};
 
 	/* @ngInject */
 	constructor(private $log: ILogService) {
 		this.log = this.$log.log;
-		this.noty = new Noty({ theme: 'metroui' });
+		this.noty = new Noty({ theme: 'metroui', timeout: 1000, closeWith: ['click'] });
 	}
 
 	public error(msg: string) {
-		this.noty.setText(msg);
-		this.noty.setType('error');
-		this.noty.show();
-
-		this.$log.error(`error: ${msg}`);
+		this.showNotification(msg, 'error');
 	}
 
 	public info(msg: string) {
-		this.noty.setText(msg);
-		this.noty.setType('info');
-		this.noty.show();
-
-		this.$log.info(`info: ${msg}`);
+		this.showNotification(msg, 'info');
 	}
 
 	public success(msg: string) {
-		this.noty.setText(msg);
-		this.noty.setType('success');
-		this.noty.show();
-
-		this.$log.info(`success: ${msg}`);
+		this.showNotification(msg, 'success');
 	}
 
 	public warning(msg: string) {
-		this.noty.setText(msg);
-		this.noty.setType('warning');
-		this.noty.show();
-
-		this.$log.warn(`warn: ${msg}`);
+		this.showNotification(msg, 'warning');
 	}
 
 	public devWarning(msg: string) {
@@ -49,5 +40,12 @@ export class Logger {
 		if (config.ENV === 'production') return;
 
 		this.warning(`[DEV] ${msg}`);
+	}
+
+	private showNotification(text: string, type: NotyType) {
+		new Noty({ type, text, theme: 'metroui', timeout: 1000, closeWith: ['click'] }).show();
+
+		const logType = this.typeMap[type];
+		(this.$log as ILogService & Indexed<Callback>)[logType](`${type}: ${text}`);
 	}
 }
