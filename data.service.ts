bb37be95d638276ca93fbc2208b6e241184d0043
Zack Schuster <email@zackschuster.com>
2017-05-19 13:59:32 -0700

    implement default returns in data service

--- data.service.ts
+++ data.service.ts
@@ -1,4 +1,4 @@
-import { IHttpPromiseCallbackArg, IHttpService, ITimeoutService } from 'angular';
+import { IHttpPromiseCallbackArg, IHttpService } from 'angular';
 import { StateService } from 'angular-ui-router';
 import { config } from 'app/core/config';
 import { Logger } from 'app/core/logger';
@@ -10,45 +10,43 @@ export class DataService {
 	/* @ngInject */
 	constructor(
 		private $http: IHttpService,
-		private $timeout: ITimeoutService,
 		private $state: StateService,
 		private logger: Logger,
 	) { }
 
-	public async Get<T = any>(url: string) {
+	public async Get<T = any>(url: string, defaultReturn: T = null) {
 		const options = Object.assign({ params: { timestamp: Date.now() } }, this.baseOptions);
-
-		try {
-			const rsp = await this.$http.get(this.prefix + url, options) as IHttpPromiseCallbackArg<{}>;
-			return rsp.data as T;
-		} catch (err) {
-			throw this.onError(err);
-		}
+		const promise = this.$http.get(this.prefix + url, options);
+		return this.safeAwait<T>(promise, defaultReturn);
 	}
 
-	public async Post<T = any>(url: string, data: T) {
+	public async Post<T = any>(url: string, data: T = null) {
+		const promise = this.$http.post(this.prefix + url, data, this.baseOptions);
+		return this.safeAwait<T>(promise);
+	}
+
+	private async safeAwait<T = any>(promise: PromiseLike<T>, defaultReturn: T = null) {
 		try {
-			const rsp = await this.$http.post(this.prefix + url, data, this.baseOptions) as IHttpPromiseCallbackArg<{}>;
-			return rsp.data as any;
+			const rsp = await promise as IHttpPromiseCallbackArg<{}>;
+			return rsp == null ? defaultReturn : rsp.data as T;
 		} catch (err) {
 			throw this.onError(err);
 		}
 	}
 
 	private onError(err: IHttpPromiseCallbackArg<{}>) {
-		this.$timeout(_ => this.checkAuthError(err.status), 300);
-		return err;
-	}
-
-	private checkAuthError(status: number) {
-		switch (status) {
+		switch (err.status) {
 			case 401:
 				this.logger.warning('You must be logged in to access this page');
 				this.$state.go('login');
-				return;
+				break;
+			case 404:
+				this.logger.devWarning(`Route '${err.config.url}' not found`);
+				break;
 			case 500:
 				this.logger.error('Internal server error. Please try again.');
-				return;
+				break;
 		}
+		return err;
 	}
 }
