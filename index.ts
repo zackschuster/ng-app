8f576753ad2585416fe7203f18bc59dc7eda70b6
Zack Schuster <email@zackschuster.com>
2017-05-19 14:35:22 -0700

    componentize footer view

--- index.ts
+++ index.ts
@@ -3,9 +3,11 @@ import 'angular-ui-router';
 
 import { DataService } from 'app/core/data.service';
 import { Logger } from 'app/core/logger';
+import { footerView } from 'app/core/layout/footer/view';
 
 export default
 	angular
 		.module('core', ['ui.router'])
 		.service('dataService', DataService)
-		.service('logger', Logger);
+		.service('logger', Logger)
+		.component('footerView', footerView);
---
+++ layout/footer/view.ts
@@ -0,0 +1,3 @@
+export const footerView = {
+	template: require('./template.pug')(),
+};
