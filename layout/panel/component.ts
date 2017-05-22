a570c87991750b1dcd2e1331a27dca94752d17e5
Zack Schuster <email@zackschuster.com>
2017-05-22 16:33:40 -0700

    mvp implementation of editablePanel component

--- layout/panel/component.ts
+++ layout/panel/component.ts
@@ -1,11 +1,32 @@
+import { copy } from 'angular';
+
+class PanelController {
+	public model: any;
+	public original: any;
+	public isEditing: boolean;
+
+	public $onInit() {
+		this.original = copy(this.model);
+		this.isEditing = false;
+	}
+
+	public restore() {
+		Object.assign(this.model, this.original);
+		this.isEditing = false;
+	}
+}
 export const editablePanel = {
 	template: require('./template.pug')(),
 	transclude: {
+		headerView: 'headerView',
+		headerEdit: 'headerEdit',
 		bodyView: 'bodyView',
-		editView: 'editView',
+		bodyEdit: 'bodyEdit',
 	},
 	bindings: {
-		ngModel: '<',
-		save: '<',
+		model: '<',
+		save: '&',
+		delete: '&',
 	},
+	controller: PanelController,
 };
