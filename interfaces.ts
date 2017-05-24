2e29484d0c7ed99d98f8c1aff68a2e0ea10985b2
Zack Schuster <email@zackschuster.com>
2017-05-24 10:14:03 -0700

    remove useless interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -1,15 +1,4 @@
-import { IAttributes, IController, IRootElementService, IScope } from 'angular';
 import { Indexed } from '@ledge/types';
-
-export interface IBaseComponentCtrl extends Indexed<IController> {
-	[index: string]: any;
-}
-
-export interface IComponentCtrl extends IBaseComponentCtrl {
-	$scope: IScope;
-	$element: IRootElementService;
-	$attrs: IAttributes;
-}
 
 export interface ICoreModel extends Indexed {
 	Id: number;
