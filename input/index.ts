54cf8b470d850b2ca184b4c136efcab39135d8e4
Zack Schuster <email@zackschuster.com>
2017-06-22 14:44:51 -0700

    remove unnecessary files

--- input/index.ts
+++
@@ -1,5 +0,0 @@
-export { checkBox } from 'core/input/checkbox/component';
-export { dateInput } from 'core/input/date/component';
-export { radioList } from 'core/input/radio/component';
-export { textBox } from 'core/input/text/box/component';
-export { textInput } from 'core/input/text/component';
--- input/text/box/component.ts
+++
@@ -1,9 +0,0 @@
-import { InputComponentOptions } from 'core/input/service';
-
-export const textBox: InputComponentOptions = {
-	type: 'input',
-	attrs: { maxlength: 3000, placeholder: '' },
-	render(h) {
-		return h.createTextArea();
-	},
-};
