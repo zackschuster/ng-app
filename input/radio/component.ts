e05f324ce59c6b7757c1465d3f3e6bcece64bf62
Zack Schuster <email@zackschuster.com>
2017-06-22 09:38:28 -0700

    radio-list: add toggle & fix label rendering

--- input/radio/component.ts
+++ input/radio/component.ts
@@ -1,5 +1,25 @@
 // tslint:disable:no-invalid-this
+import { CoreInputController } from 'core/input/controller';
 import { defineInputComponent } from 'core/input/definition';
+
+class RadioListController extends CoreInputController {
+	public $postLink() {
+		this.$timeout(_ => {
+			const el = document.querySelector('[ng-transclude="contain"]');
+			if (el.innerHTML === '') {
+				el.remove();
+			}
+		});
+	}
+
+	public toggle(value: any) {
+		if (this.ngModel === value) {
+			this.ngModel = null;
+		} else {
+			this.ngModel = value;
+		}
+	}
+}
 
 export const radioList = defineInputComponent({
 	templateClass: 'form-check',
@@ -7,18 +27,28 @@ export const radioList = defineInputComponent({
 	nestInputInLabel: true,
 	render(h) {
 		const radio = h.createInput('radio');
+
+		radio.removeAttribute('ng-model');
 		radio.setAttribute('ng-value', '{{item.Value}}');
+		radio.setAttribute('ng-checked', '$ctrl.ngModel == item.Value');
+		radio.setAttribute('ng-click', '$ctrl.toggle(item.Value)');
 
 		this.$template.setAttribute('ng-repeat', 'item in $ctrl.list');
 
 		return radio;
 	},
 	renderLabel() {
+		const label = this.$template.querySelector('label');
 		const labelText = document.createTextNode('{{item.Text}}');
-		this.$template.querySelector('label').appendChild(labelText);
+		const spanTag = document.createElement('span');
+
+		spanTag.appendChild(labelText);
+		label.setAttribute('for', '{{id}}{{$index}}');
+		label.appendChild(spanTag);
 	},
 	bindings: {
 		list: '<',
 		ngChecked: '<',
 	},
+	controller: RadioListController,
 });
