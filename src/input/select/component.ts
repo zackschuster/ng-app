3a361688b0b22c19bf428f68a1863fb79ea1c723
Zack Schuster <email@zackschuster.com>
2017-07-11 16:14:38 -0700

    updates and bugfixes for select component

--- src/input/select/component.ts
+++ src/input/select/component.ts
@@ -1,16 +1,23 @@
 import 'element-closest';
 import Choices from 'choices.js';
+import { IAttributes } from 'angular';
 import { NgController } from '../../controller';
 import { InputComponentOptions } from '../../..';
+
+function isMultiple($attrs: IAttributes) {
+	return $attrs.hasOwnProperty('multiple') || $attrs.type === 'multiple';
+}
 
 class SelectController extends NgController {
 	public list: any[];
 	public choices: Choices;
+	private isMultiple: boolean;
 
 	public $postLink() {
 		const $el = (this.$element as any)[0] as HTMLElement;
-		const $select = $el.querySelector('select,input') as HTMLElement;
+		const $select = $el.querySelector('select') as HTMLElement;
 
+		this.isMultiple = isMultiple(this.$attrs);
 		this.makeSelectList($select, this.list);
 
 		this.$scope.$watch(
@@ -29,23 +36,40 @@ class SelectController extends NgController {
 			this.choices.destroy();
 		}
 
-		this.choices = new Choices(el, { removeItemButton: true });
-		this.choices.setChoices(list, this.$attrs.value || 'Value', this.$attrs.text || 'Text');
-		this.choices.passedElement.addEventListener('change', this.changeEvent);
+		const value = this.$attrs.value || 'Value';
+		const text = this.$attrs.text || 'Text';
+
+		this.choices = new Choices(el, {
+			removeItemButton: true,
+			searchFields: [text],
+			itemSelectText: '',
+			placeholderValue: this.$attrs.placeholder,
+		});
+
+		this.choices.setChoices(list, value, text);
+		this.choices.passedElement.addEventListener('change', this.changeEvent.bind(this));
 	}
 
 	public changeEvent(event: any) {
-		this.ngModel = event.detail.value;
+		const { value } = event.detail;
+		if (this.ngModel != null && this.isMultiple ? this.ngModel.includes(value) : this.ngModel === value) {
+			return;
+		}
+		this.ngModel = this.isMultiple ? [value].concat(this.ngModel || []) : value;
 	}
 }
 
 export const selectList: InputComponentOptions = {
 	type: 'input',
 	render(h) {
+		const input = h.createElement('select');
+
 		// tslint:disable-next-line:no-invalid-this
-		return this.$attrs.hasOwnProperty('multiple') || this.$attrs.type === 'multiple'
-			? h.createInput('select-multiple')
-			: h.createElement('select');
+		if (isMultiple(this.$attrs)) {
+			input.setAttribute('multiple', 'true');
+		}
+
+		return input;
 	},
 	ctrl: SelectController,
 	bindings: {
