cee641188d0edefd8eae5ab507844aa32f45d91c
Zack Schuster <email@zackschuster.com>
2017-08-23 14:21:00 -0700

    add min/step attr configuration to textInput

--- src/input/text/component.ts
+++ src/input/text/component.ts
@@ -1,3 +1,4 @@
+// tslint:disable:no-invalid-this
 import { InputComponentOptions } from '../../..';
 
 export const textInput: InputComponentOptions = {
@@ -5,7 +6,13 @@ export const textInput: InputComponentOptions = {
 	canHaveIcon: true,
 	attrs: { maxlength: 3000, placeholder: '' },
 	render(h) {
-		// tslint:disable-next-line:no-invalid-this
-		return h.createInput(this.$attrs.type);
+		const input = h.createInput(this.$attrs.type);
+
+		if (input.type === 'number') {
+			input.setAttribute('min', this.$attrs.min || '1');
+			input.setAttribute('step', this.$attrs.step || 'any');
+		}
+
+		return input;
 	},
 };
