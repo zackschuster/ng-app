31ac84c10d332ca71247386441d4d440a2a3b828
Zack Schuster <email@zackschuster.com>
2018-05-17 10:59:47 -0700

    test: update renderer tests

--- test/renderer.ts
+++ test/renderer.ts
@@ -7,33 +7,35 @@ const h = new NgRenderer(new (require('window'))().document);
 test('renderer creates bare element', t => {
 	const div = h.createElement('div');
 	t.true(div.classList.length === 0);
-	t.true(div.attributes.length === 1);
+	t.true(div.attributes.length === 0);
 });
 
 test('renderer creates element with classes', t => {
 	const div = h.createElement('div', ['test1', 'test2']);
-	t.true(div.classList.length === 2);
+	t.true(div.classList.contains('test1'));
+	t.true(div.classList.contains('test2'));
 	t.true(div.attributes.length === 1);
 });
 
 test('renderer creates element with attributes', t => {
 	const div = h.createElement('div', [], [['test1', 'test1'], ['test2', 'test2']]);
 	t.true(div.classList.length === 0);
-	t.true(div.attributes.length === 3);
+	t.true(div.attributes.getNamedItem('test1') != null);
+	t.true(div.attributes.getNamedItem('test2') != null);
 });
 
 test('renderer creates element with classes and attributes', t => {
 	const div = h.createElement('div', ['test1', 'test2'], [['test1', 'test1'], ['test2', 'test2']]);
-	t.true(div.classList.length === 2);
+	t.true(div.classList.contains('test1'));
+	t.true(div.classList.contains('test2'));
 	t.true(div.attributes.length === 3);
 });
 
 test('renderer creates text input by default', t => {
 	const input = h.createInput();
 	t.true(input.type === 'text');
-	t.true(input.classList.length === 1);
 	t.true(input.classList.contains('form-control'));
-	t.true(input.attributes.length === 7);
+	t.true(input.attributes.length === 8);
 });
 
 test('renderer creates radio and checkbox inputs', t => {
@@ -41,13 +43,13 @@ test('renderer creates radio and checkbox inputs', t => {
 	t.true(radio.type === 'radio');
 	t.true(radio.classList.length === 1);
 	t.true(radio.classList.contains('form-check-input'));
-	t.true(radio.attributes.length === 5);
+	t.true(radio.attributes.length === 6);
 
 	const checkbox = h.createInput('checkbox');
 	t.true(checkbox.type === 'checkbox');
 	t.true(checkbox.classList.length === 1);
 	t.true(checkbox.classList.contains('form-check-input'));
-	t.true(checkbox.attributes.length === 5);
+	t.true(checkbox.attributes.length === 6);
 });
 
 test('renderer creates textarea', t => {
@@ -55,7 +57,7 @@ test('renderer creates textarea', t => {
 	t.true(input.tagName.toLowerCase() === 'textarea');
 	t.true(input.classList.length === 1);
 	t.true(input.classList.contains('form-control'));
-	t.true(input.attributes.length === 7);
+	t.true(input.attributes.length === 8);
 });
 
 test('renderer creates icon', t => {
@@ -92,7 +94,7 @@ test('renderer creates slot', t => {
 	const label = h.createSlot('test');
 	t.true(label.tagName.toLowerCase() === 'div');
 	t.true(label.classList.length === 0);
-	t.true(label.attributes.length === 2);
+	t.true(label.attributes.length === 1);
 	t.true(label.getAttribute('ng-transclude') === 'test');
 });
 
@@ -100,5 +102,6 @@ test('renderer creates icon input', t => {
 	const input = h.createInput();
 	const iconInput = h.createIconInput(input, 'calendar');
 	t.true(iconInput.querySelectorAll('.fa-calendar').length > 0);
-	t.true(iconInput.querySelector('input').isEqualNode(input));
+	// tslint:disable-next-line:no-non-null-assertion
+	t.true(iconInput.querySelector('input')!.isEqualNode(input));
 });
