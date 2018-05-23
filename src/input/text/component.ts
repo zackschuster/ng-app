a97cd30e52c5167a4f7a39e2ba8d9c1d539abf2c
Zack Schuster <email@zackschuster.com>
2018-05-23 16:16:24 -0700

    input/text: properly validate min/max

--- src/input/text/component.ts
+++ src/input/text/component.ts
@@ -1,4 +1,29 @@
 import { InputComponentOptions } from '../../..';
+import { NgComponentController } from '../../controller';
+
+class TextInputController extends NgComponentController {
+	public min: number | undefined;
+	public max: number | undefined;
+
+	public $onInit() {
+		if (this.$attrs.type === 'number') {
+			this.ngModelCtrl.$validators.minVal = val => {
+				const valid = this.min == null || val >= this.min;
+				if (valid === false) {
+					this.ngModelCtrl.$setTouched();
+				}
+				return valid;
+			};
+			this.ngModelCtrl.$validators.maxVal = val => {
+				const valid = this.max == null || val <= this.max;
+				if (valid === false) {
+					this.ngModelCtrl.$setTouched();
+				}
+				return valid;
+			};
+		}
+	}
+}
 
 export const textInput: InputComponentOptions = {
 	type: 'input',
@@ -8,10 +33,21 @@ export const textInput: InputComponentOptions = {
 		const input = h.createInput(this.$attrs.type);
 
 		if (input.type === 'number') {
-			input.setAttribute('min', this.$attrs.min || '1');
-			input.setAttribute('step', this.$attrs.step || 'any');
+			input.setAttribute('ng-attr-min', '{{$ctrl.min}}');
+			input.setAttribute('ng-attr-max', '{{$ctrl.max}}');
+			input.setAttribute('ng-attr-step', `{{$ctrl.step || 'any'}}`);
 		}
 
 		return input;
 	},
+	bindings: {
+		min: '<',
+		max: '<',
+		step: '<',
+	},
+	validators: new Map<string, string>([
+		['minVal', 'Must be greater than or equal to {{$ctrl.min}}'],
+		['maxVal', 'Must be less than or equal to {{$ctrl.max}}'],
+	]),
+	ctrl: TextInputController,
 };
