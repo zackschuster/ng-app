adaf663952f357522ac4136d94d689fa36936b68
Zack Schuster <email@zackschuster.com>
2018-06-21 11:26:16 -0700

    index: improve efficiency

--- index.ts
+++ index.ts
@@ -1,16 +1,15 @@
 import * as inputs from './src/input';
 import { NgApp } from './src/app';
-import { NgComponentController, NgController } from './src/controller';
-import { NgRenderer } from './src/renderer';
 
-const $inputs = new Map(Object.entries(inputs));
-export const app = new NgApp().registerComponents($inputs);
+export const app = new NgApp().registerComponents(inputs);
 
 export function wrapCtrl(controller: new() => any) {
 	return app._wrapComponentController(controller);
 }
 
+export { NgApp };
+export { NgComponentController, NgController } from './src/controller';
 export { NgDataService } from './src/http';
 export { NgLogger } from './src/logger';
 export { NgModalOptions, NgModalService } from './src/modal';
-export { NgController, NgComponentController, NgRenderer };
+export { NgRenderer } from './src/renderer';
