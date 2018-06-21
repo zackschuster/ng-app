b494d9ab9c24ee5352fcd1fe6102901e5a4a93b4
Zack Schuster <email@zackschuster.com>
2018-06-21 15:40:34 -0700

    http: add test for getFullUrl

--- test/http.ts
+++ test/http.ts
@@ -1,5 +1,5 @@
 import test from 'ava';
-import { $http } from './mocks';
+import { $http, $prefix } from './mocks';
 // import { $backend, $http } from './mocks';
 
 test('http has appropriate methods', t => {
@@ -11,8 +11,12 @@ test('http has appropriate methods', t => {
 	t.is(typeof $http.Jsonp, 'function');
 });
 
+test('getFullUrl returns prefixed url', t => {
+	t.is($http.getFullUrl('test'), `${$prefix}/test`);
+});
+
 // fails with "promise not resolved" error
 // test('http get', async t => {
-// 	$backend.whenGET($http.getFullUrl('test')).respond('success');
+// 	$backend.whenGET($http.getFullUrl('test')).respond(200, 'success');
 // 	t.is(await $http.Get<string>('test'), 'success');
 // });
--- test/mocks/index.ts
+++ test/mocks/index.ts
@@ -1,3 +1,3 @@
 export { logger as $log } from './_logger';
-export { http as $http, backend as $backend } from './_http';
+export { http as $http, backend as $backend, prefix as $prefix } from './_http';
 export { h } from './_renderer';
