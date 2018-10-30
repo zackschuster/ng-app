383ae09d9bf626f32c7f3bc89d7ad08982f4e752
Zack Schuster <email@zackschuster.com>
2018-10-30 13:56:58 -0700

    logger: in top shape now

--- src/logger.ts
+++ src/logger.ts
@@ -1,78 +1,122 @@
 import * as Noty from 'noty';
-import { Callback, Indexed } from '@ledge/types';
+import { Callback } from '@ledge/types';
 import { NgService } from './service';
 
-export class NgLogger extends NgService {
-	public log: angular.ILogCall;
-	private typeMap: {
-		warning: 'warn',
-		success: 'log',
-		information: 'info',
-		info: 'info',
-		error: 'error',
-		alert: 'error',
-	} = {
-		warning: 'warn',
-		success: 'log',
-		information: 'info',
-		info: 'info',
-		error: 'error',
-		alert: 'error',
-	};
+export enum LogTypeMap {
+	warning = 'warn',
+	success = 'log',
+	information = 'info',
+	info = 'info',
+	error = 'error',
+	alert = 'error',
+}
 
+type Type = Noty.Type;
+type Button = Noty.Button;
+
+export class NgLogger extends NgService {
 	constructor(private $log: angular.ILogService, private isProd = false) {
 		super();
-		this.log = this.$log.log;
 	}
 
+	/**
+	 * Clear all active notifications
+	 */
 	public clear() {
 		Noty.closeAll();
 	}
 
+	/**
+	 * Prompt the user to confirm intent for a previous action
+	 */
 	public confirm(action: Callback, msg = 'Please confirm your action') {
-		const n = new Noty({
-			type: 'alert',
-			theme: 'metroui',
-			text: '<p class="h5 mb-0 ml-0">\
-			<i class="fa fa-exclamation-triangle text-warning mr-1"></i>' + msg + '</p>',
-			buttons: [
-				Noty.button('No', 'btn btn-outline-danger mb-2 pull-right', (_: any) => n.close()),
-				Noty.button('Yes', 'btn btn-outline-success mr-1 pull-right', (_: any) => {
-					action();
-					n.close();
-				}),
-			],
-		});
+		const text = '<p class="h5 mb-0 ml-0">\
+		<i class="fa fa-exclamation-triangle text-warning mr-1"></i>' + msg + '</p>';
 
-		n.show();
+		const noty = this.notify(text, 'alert', false, [
+			Noty.button('No', 'btn btn-outline-danger mb-2 pull-right', () => noty.close()),
+			Noty.button('Yes', 'btn btn-outline-success mr-1 pull-right', () => {
+				action();
+				noty.close();
+			}),
+		]);
 	}
 
-	public error(msg: string, isTemporary = false) {
-		this.showNotification(msg, 'error', isTemporary && undefined);
+	/**
+	 * Display an error notification
+	 *
+	 * @param text Display text
+	 * @param isTemporary Whether the notification should disappear automatically (false by default)
+	 */
+	public error(text: string, isTemporary = false) {
+		this.notify(text, 'error', isTemporary && undefined);
 	}
 
-	public info(msg: string, isTemporary = true) {
-		this.showNotification(msg, 'info', isTemporary && undefined);
+	/**
+	 * Display an informational notification
+	 *
+	 * @param text Display text
+	 * @param isTemporary Whether the notification should disappear automatically (true by default)
+	 */
+	public info(text: string, isTemporary = true) {
+		this.notify(text, 'info', isTemporary && undefined);
 	}
 
-	public success(msg: string, isTemporary = true) {
-		this.showNotification(msg, 'success', isTemporary && undefined);
+	/**
+	 * Display a success notification
+	 *
+	 * @param text Display text
+	 * @param isTemporary Whether the notification should disappear automatically (true by default)
+	 */
+	public success(text: string, isTemporary = true) {
+		this.notify(text, 'success', isTemporary && undefined);
 	}
 
-	public warning(msg: string, isTemporary = true) {
-		this.showNotification(msg, 'warning', isTemporary && undefined);
+	/**
+	 * Display a warning notification
+	 *
+	 * @param text Display text
+	 * @param isTemporary Whether the notification should disappear automatically (true by default)
+	 */
+	public warning(text: string, isTemporary = true) {
+		this.notify(text, 'warning', isTemporary && undefined);
 	}
 
-	public devWarning(msg: string) {
+	/**
+	 * Display a warning notification (disabled when `process.env.NODE_ENV === 'production'`)
+	 *
+	 * @param text Display text
+	 */
+	public devWarning(text: string) {
 		if (this.isProd === false) {
-			this.warning(`[DEV] ${msg}`, false);
+			this.warning(`[DEV] ${text}`, false);
 		}
 	}
 
-	private showNotification(text: string, type: Noty.Type, timeout: false | number = 1000) {
-		new Noty({ type, text, theme: 'metroui', progressBar: false, timeout, closeWith: ['click'] }).show();
+	/**
+	 * Show a customizable notification to the user
+	 *
+	 * @param text Display text
+	 * @param type Display type
+	 * @param timeout Length in ms before notification disappears (`false` to set permanently)
+	 * @param buttons Interaction points for the user
+	 */
+	public notify(text: string, type: Type, timeout: false | number = 1000, buttons: Button[] = []) {
+		const logType = LogTypeMap[type];
+		this.$log[logType](`${type}: ${text}`);
 
-		const logType = this.typeMap[type];
-		(this.$log as angular.ILogService & Indexed<Callback>)[logType](`${type}: ${text}`);
+		const noty = new Noty({
+			type,
+			text,
+			theme: 'metroui',
+			progressBar: false,
+			timeout,
+			buttons,
+			closeWith: ['click'],
+		});
+
+		noty.show();
+
+		return noty;
 	}
 }
