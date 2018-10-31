af66a264a01407f922f97d5dca83b7422086730f
Zack Schuster <email@zackschuster.com>
2018-10-30 17:22:37 -0700

    http: support request & response interceptors

--- src/http.ts
+++ src/http.ts
@@ -8,49 +8,49 @@ export class NgDataService extends NgService {
 		private interceptors: angular.IHttpInterceptor[],
 		private update: () => void,
 		private prefixGetter: () => string,
-		private baseOptions: angular.IRequestShortcutConfig = { withCredentials: true },
+		private options: angular.IRequestShortcutConfig = { withCredentials: true },
 	) {
 		super();
-
-		this.$http.defaults = Object.assign(
-			this.$http.defaults, {
-				params: {
-					timestamp: (this.isIE11 ? Date.now() : null),
-				},
-			}, this.baseOptions);
 	}
 
 	public async Get<T = any>(url: string) {
-		return this.wrapQPromise(this.$http.get<T>(this.getFullUrl(url)));
+		url = this.getFullUrl(url);
+		return this.fulfillRequest(
+			this.$http.get<T>(url, await this.getRequestConfig('GET', url)),
+		);
 	}
 
 	public async Post<T = any>(url: string, data: any = null) {
-		return this.wrapQPromise(
-			this.$http.post<T>(this.getFullUrl(url), data),
+		url = this.getFullUrl(url);
+		return this.fulfillRequest(
+			this.$http.post<T>(url, data, await this.getRequestConfig('POST', url)),
 		);
 	}
 
 	public async Patch<T = any>(url: string, data: PatchPayload) {
-		return this.wrapQPromise(
-			this.$http.patch<T>(this.getFullUrl(url), data),
+		url = this.getFullUrl(url);
+		return this.fulfillRequest(
+			this.$http.patch<T>(url, data, await this.getRequestConfig('PATCH', url)),
 		);
 	}
 
 	public async Put<T = any>(url: string, data: T) {
-		return this.wrapQPromise(
-			this.$http.put<T>(this.getFullUrl(url), data),
+		url = this.getFullUrl(url);
+		return this.fulfillRequest(
+			this.$http.put<T>(url, data, await this.getRequestConfig('PUT', url)),
 		);
 	}
 
 	public async Delete<T = any>(url: string) {
-		return this.wrapQPromise(
-			this.$http.delete<T>(this.getFullUrl(url)),
+		url = this.getFullUrl(url);
+		return this.fulfillRequest(
+			this.$http.delete<T>(url, await this.getRequestConfig('DELETE', url)),
 		);
 	}
 
 	public async Jsonp<T = any>(url: string) {
-		return this.wrapQPromise(
-			this.$http.jsonp<T>(this.getFullUrl(url)),
+		return this.fulfillRequest(
+			this.$http.jsonp<T>(url, await this.getRequestConfig('JSONP', url)),
 		);
 	}
 
@@ -60,14 +60,37 @@ export class NgDataService extends NgService {
 		return `${prefix}${hasSlash ? '' : '/'}${endpoint}`;
 	}
 
-	private async wrapQPromise<T>(promise: angular.IHttpPromise<T>) {
+	private async getRequestConfig(method: string, url: string) {
+		let options = Object.assign({
+			method,
+			url,
+			params: {
+				timestamp: (this.isIE11 ? Date.now() : null),
+			},
+		}, this.options);
+
+		for (const onRequest of this.interceptors.map(x => x.request)) {
+			if (isFunction(onRequest)) {
+				options = await onRequest(options) as typeof options;
+			}
+		}
+
+		return options;
+	}
+
+	private async fulfillRequest<T>(promise: angular.IHttpPromise<T>) {
 		try {
-			const { data } = await promise;
-			return data;
+			let response = await promise;
+			for (const onResponse of this.interceptors.map(x => x.response)) {
+				if (isFunction(onResponse)) {
+					response = await onResponse<T>(response);
+				}
+			}
+			return response.data;
 		} catch (err) {
 			for (const onResponseError of this.interceptors.map(x => x.responseError)) {
 				if (isFunction(onResponseError)) {
-					err = await onResponseError(err);
+					err = await onResponseError<T>(err);
 				}
 			}
 			throw err;
