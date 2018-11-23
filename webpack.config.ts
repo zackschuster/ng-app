293994b38fd08ea3de98b64e2719a040eaf0c35b
Zack Schuster <email@zackschuster.com>
2018-11-22 18:52:50 -0800

    docs: initial working code

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,8 +5,9 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const context = process.cwd();
 
+// @ts-ignore
 module.exports = (env = 'development') => {
-	const config = require('@ledge/configs/webpack.merge')(env, {});
+	const config = require('@ledge/configs/webpack.merge')(env, { });
 
 	config.entry.app = [
 		resolve(context, 'docs', 'app.ts'),
@@ -14,27 +15,6 @@ module.exports = (env = 'development') => {
 	];
 
 	config.resolve.modules = ['.', 'docs', 'node_modules'];
-
-	config.module.rules.splice(
-		config.module.rules.findIndex((x: { test: RegExp}) => x.test.source === '\\.ts$'),
-		1,
-		{
-			test: /\.ts$/,
-			use: {
-				loader: require.resolve('ts-loader'),
-				options: {
-					context: process.cwd(),
-					transpileOnly: true,
-					onlyCompileBundledFiles: true,
-					compilerOptions: Object.assign(
-						require('@ledge/configs/tsconfig.base').compilerOptions,
-						require('@ledge/configs/tsconfig.browser').compilerOptions,
-						{ target: 'es2018', esModuleInterop: true },
-					),
-				},
-			},
-		},
-	);
 
 	config.plugins.push(
 		new HtmlWebpackPlugin({
