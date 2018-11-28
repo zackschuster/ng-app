4cf81ed2a6632d1aad11568a48af9864ef601e57
Zack Schuster <email@zackschuster.com>
2018-11-28 04:37:21 -0800

    docs: update webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,56 +1,20 @@
 // tslint:disable:no-var-requires
-const { resolve } = require('path');
+const { join } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const context = process.cwd();
-
-module.exports = (env = 'development') => {
-	const config = require('@ledge/configs/webpack.merge')(env, {});
-
-	config.entry.app = [
-		resolve(context, 'docs', 'app.ts'),
-		resolve(context, 'docs', 'styles.scss'),
-	];
-
-	config.resolve.modules = ['.', 'docs', 'node_modules'];
-
-	config.module.rules.splice(
-		config.module.rules.findIndex((x: { test: RegExp}) => x.test.source === '\\.ts$'),
-		1,
-		{
-			test: /\.ts$/,
-			use: {
-				loader: require.resolve('ts-loader'),
-				options: {
-					context: process.cwd(),
-					transpileOnly: true,
-					onlyCompileBundledFiles: true,
-					compilerOptions: Object.assign(
-						require('@ledge/configs/tsconfig.base').compilerOptions,
-						require('@ledge/configs/tsconfig.browser').compilerOptions,
-						{ target: 'es2018', esModuleInterop: true },
-					),
-				},
-			},
+module.exports = (env = 'development') =>
+	require('@ledge/configs/webpack.merge')(env, {
+		entry: {
+			app: ['app.ts', 'styles.scss'].map(x => join(process.cwd(), 'docs', x)),
 		},
-	);
-
-	config.plugins.push(
-		new HtmlWebpackPlugin({
-			template: '!!pug-loader?!docs/index.pug',
-			title: '@ledge/ng-app docs',
-			baseUrl: '/',
-		}),
-	);
-
-	if (env !== 'development') {
-		config.plugins.push(
-			new CopyWebpackPlugin([
-				{ context: 'src', from: 'images/**/*' },
-			]),
-		);
-	}
-
-	return config;
-};
+		resolve: {
+			modules: ['docs'],
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: '!!pug-loader?!docs/index.pug',
+				title: '@ledge/ng-app docs',
+				baseUrl: '/',
+			}),
+		],
+	});
