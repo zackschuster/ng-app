3c3458a7832d97e3278fa06fa468d98d407a4055
Zack Schuster <email@zackschuster.com>
2018-11-28 14:11:20 -0800

    docs: ship initial code

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,17 +2,22 @@
 const { join } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const docs = join(process.cwd(), 'docs');
+
 module.exports = (env = 'development') =>
 	require('@ledge/configs/webpack.merge')(env, {
 		entry: {
-			app: ['app.ts', 'styles.scss'].map(x => join(process.cwd(), 'docs', x)),
+			app: ['app.ts', 'styles.scss'].map(x => join(docs, 'src', x)),
+		},
+		output: {
+			path: env !== 'development' ? docs : process.cwd(),
 		},
 		resolve: {
 			modules: ['docs'],
 		},
 		plugins: [
 			new HtmlWebpackPlugin({
-				template: '!!pug-loader?!docs/index.pug',
+				template: '!!pug-loader?!docs/src/index.pug',
 				title: '@ledge/ng-app docs',
 				baseUrl: '/',
 			}),
