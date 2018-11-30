848e1c43b23384c9caf95478ab3f00c61a086d6a
Zack Schuster <email@zackschuster.com>
2018-11-29 20:31:28 -0800

    docs/build: inline baseUrl

--- webpack.config.ts
+++ webpack.config.ts
@@ -33,7 +33,6 @@ module.exports = (env = 'development') =>
 			new HtmlWebpackPlugin({
 				template: '!!pug-loader?!docs/src/index.pug',
 				title: '@ledge/ng-app docs',
-				baseUrl: '/',
 			}),
 			new NgAppDocsPlugin(),
 		],
