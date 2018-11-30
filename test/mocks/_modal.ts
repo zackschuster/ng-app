e4764bd291e417a2a09050ffcb7e4cfcfa2f57ab
Zack Schuster <email@zackschuster.com>
2018-11-30 08:38:48 -0800

    test/modal: fix mock

--- test/mocks/_modal.ts
+++ test/mocks/_modal.ts
@@ -1,11 +1,8 @@
 import { NgModalService } from '../../src/modal';
 import { $injector } from './__injector';
-import { $http } from './_http';
 import { $log } from './_logger';
 
 export const $modal = new NgModalService(
 	$injector.get('$uibModal'),
-	$injector.get('$timeout'),
-	$http,
 	$log,
 );
