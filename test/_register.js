7d3c5faeeda5f816bcd6768ab30bd88182e23eae
Zack Schuster <email@zackschuster.com>
2018-12-01 09:53:07 -0800

    test: remove crypto polyfill

--- test/_register.js
+++ test/_register.js
@@ -11,27 +11,4 @@ Object.defineProperty(global, 'angular', {
 	writable: false,
 	value: /** @type {*} */(window).angular,
 });
-
-// taken from https://github.com/PeculiarVentures/node-webcrypto-ossl/blob/6ca13d14243901bb52c195ee0fee70bc4e58fd84/lib/webcrypto.ts#L39
-Object.defineProperty(global, 'crypto', {
-	configurable: false,
-	enumerable: true,
-	writable: false,
-	value: {
-		getRandomValues(array) {
-			if (array) {
-				if (array.byteLength > 65536) {
-					const error = new Error(`ERR_RANDOM_VALUE_LENGTH ${array.byteLength}`);
-					/** @type {*} */
-					(error).code = 22;
-					throw error;
-				}
-				const bytes = require('crypto').randomBytes(array.byteLength);
-				array.set(new (array.constructor)(bytes.buffer));
-				return array;
-			}
-			return null;
-		},
-	},
-});
 require('angular-ui-bootstrap');
