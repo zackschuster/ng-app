b385864aa41b953301223d668d632e61f9ae407b
Zack Schuster <email@zackschuster.com>
2018-11-30 21:40:14 -0800

    ngservice: add UUIDv4 function

--- src/services/base.ts
+++ src/services/base.ts
@@ -4,9 +4,24 @@ export class NgService {
 	public static IsMobile() {
 		return typeof window === 'object' && window.innerWidth < 767;
 	}
+
+	/**
+	 * @see https://stackoverflow.com/a/2117523
+	 */
+	public static UUIDv4() {
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+			// tslint:disable:no-bitwise
+			const r = Math.random() * 16 | 0;
+			const v = c === 'x' ? r : (r & 0x3 | 0x8);
+			// tslint:enable:no-bitwise
+			return v.toString(16);
+		});
+	}
+
 	protected get isMobile() {
 		return NgService.IsMobile();
 	}
+
 	protected get isIE11() {
 		return isIE11();
 	}
