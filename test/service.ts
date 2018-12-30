f51cd8672359c6d724108d7f8a83f5210277e148
Zack Schuster <email@zackschuster.com>
2018-12-30 15:06:06 -0800

    services/base: add special-case exceptions to splitByCapitalLetter

--- test/service.ts
+++ test/service.ts
@@ -6,4 +6,11 @@ test('$svc.splitByCapitalLetter', async t => {
 	t.is($svc.splitByCapitalLetter('splitBy CapitalLetter'), 'Split By Capital Letter');
 	t.is($svc.splitByCapitalLetter('splitBy capitalLetter'), 'Split By capital Letter');
 	t.is($svc.splitByCapitalLetter('splitBycapitalLetter'), 'Split Bycapital Letter');
+	t.is($svc.splitByCapitalLetter('exampleFAQ'), 'Example FAQ');
+	t.is($svc.splitByCapitalLetter('exampleF.A.Q.'), 'Example F.A.Q.');
+	t.is($svc.splitByCapitalLetter('exampleF.A.Q.Replies'), 'Example F.A.Q. Replies');
+	t.is($svc.splitByCapitalLetter('exampleF.A.Q.replies'), 'Example F.A.Q. Replies');
+	t.is($svc.splitByCapitalLetter('FBIstatistics'), 'FBIstatistics');
+	t.is($svc.splitByCapitalLetter('F.B.I.Statistics'), 'F.B.I. Statistics');
+	t.is($svc.splitByCapitalLetter('F.B.I.statistics'), 'F.B.I. Statistics');
 });
