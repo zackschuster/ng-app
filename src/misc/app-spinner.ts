0c07cfea51ceb32be9145dfabd6edc81a1feca76
Zack Schuster <email@zackschuster.com>
2019-02-11 09:44:11 -0800

    misc: implement app-spinner

---
+++ src/misc/app-spinner.ts
@@ -0,0 +1,27 @@
+import { NgController } from '../controller';
+import { NgComponentOptions } from '../options';
+import { NgRenderer } from '../services/renderer';
+
+class SpinnerController extends NgController {
+	private readonly colors = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'];
+
+	public $onInit() {
+		const { color = 'primary' } = this.$attrs;
+
+		if (this.colors.includes(color) === false) {
+			throw new Error(`Icon not supported: ${color}.\nSupported colors: ${this.colors.join(', ')}`);
+		}
+
+		const renderer = new NgRenderer(document);
+		const spinner = renderer.createHtmlElement('div', ['spinner-border', `text-${color}`, 'mx-1'], [['role', 'status']]);
+		const srDesc = renderer.createHtmlElement('span', ['sr-only']);
+		srDesc.innerText = 'Loading...';
+		spinner.appendChild(srDesc);
+
+		this.$element.appendChild(spinner);
+	}
+}
+
+export const appSpinner: NgComponentOptions = {
+	ctrl: SpinnerController,
+};
--- src/misc/index.ts
+++ src/misc/index.ts
@@ -1,5 +1,7 @@
 import { appIcon } from './app-icon';
+import { appSpinner } from './app-spinner';
 
 export const misc = {
 	appIcon,
+	appSpinner,
 };
