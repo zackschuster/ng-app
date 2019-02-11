b3c0b74f1ac8da9b090537a661ebed325fc78b61
Zack Schuster <email@zackschuster.com>
2019-02-11 08:16:07 -0800

    chore: update polyfills

--- polyfills.ts
+++ polyfills.ts
@@ -3,6 +3,7 @@ import 'core-js/es6/symbol';
 import 'core-js/es6/map';
 import 'core-js/es6/set';
 import 'core-js/es6/promise';
+import 'core-js/fn/array/find';
 import 'core-js/fn/array/from';
 import 'core-js/fn/array/includes';
 import 'core-js/fn/number/is-integer';
@@ -12,7 +13,13 @@ import 'core-js/fn/object/entries';
 import 'core-js/fn/string/includes';
 import 'core-js/fn/string/starts-with';
 
-// element-closest | CC0-1.0 | github.com/jonathantneal/closest
+/**
+ * @package element-closest
+ * @license CC0-1.0
+ * @author Jonathan T Neal
+ * @see https://github.com/jonathantneal/closest
+ */
+
 Element.prototype.matches = function matches(selector: string) {
 	var element = this;
 	var elements = (element.ownerDocument as Document).querySelectorAll(selector);
