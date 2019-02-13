d461646a52454556c0934b247232cdc87b8948f4
Zack Schuster <email@zackschuster.com>
2019-02-13 11:35:50 -0800

    inputs/select-list: remove $timeout references

--- src/inputs/select-list.ts
+++ src/inputs/select-list.ts
@@ -106,7 +106,7 @@ class SelectController extends NgInputController {
 				? [value].concat(this.ngModel == null ? [] : this.ngModel)
 				: value;
 
-			this.$timeout();
+			this.$scope.$applyAsync();
 		});
 
 		choices.passedElement.element.addEventListener('removeItem', ({ detail: { value } }) => {
@@ -117,7 +117,7 @@ class SelectController extends NgInputController {
 			} else {
 				this.ngModel = undefined;
 			}
-			this.$timeout();
+			this.$scope.$applyAsync();
 		});
 
 		const input = this.$element.querySelector('input') as HTMLInputElement;
