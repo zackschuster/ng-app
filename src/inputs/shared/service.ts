49308c95153717e21d6f181da21f657e2b0ad7d9
Zack Schuster <email@zackschuster.com>
2019-02-13 16:17:40 -0800

    services: assign uniqueId to all derivatives of NgService

--- src/inputs/shared/service.ts
+++ src/inputs/shared/service.ts
@@ -91,7 +91,7 @@ export class InputService extends NgService {
 	}
 
 	public static wrapComponentCtrl($ctrl: new(...args: any[]) => NgInputController) {
-		return class extends $ctrl {
+		return class WrappedInputController extends $ctrl {
 			constructor() {
 				super();
 				setTimeout(() => {
