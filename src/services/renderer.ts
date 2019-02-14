06ff8de5acfc2dc54a705147930d66e3ac8feb82
Zack Schuster <email@zackschuster.com>
2019-02-14 13:28:40 -0800

    renderer: update implementation to match new angular api

--- src/services/renderer.ts
+++ src/services/renderer.ts
@@ -1,3 +1,5 @@
+import { WritableKeysOf } from '@ledge/types';
+
 export class NgRenderer {
 	public baseInputAttrs: [string, string][] = [
 		['ng-attr-id', '{{id}}_{{$ctrl.uniqueId}}'],
@@ -6,7 +8,7 @@ export class NgRenderer {
 		['ng-model-options', '$ctrl.ngModelOptions'],
 	];
 
-	constructor(private document: Document) { }
+	constructor(public readonly document: Document) { }
 
 	public createHtmlElement<T extends keyof HTMLElementTagNameMap | 'ng-transclude'>(
 		tagName: T,
@@ -73,14 +75,14 @@ export class NgRenderer {
 	}
 
 	public createLabel(classList: string[], {
-			isRequired = false,
-			isSrOnly = false,
-			isRadio = false,
-		}: {
-			isRequired?: boolean,
-			isSrOnly?: boolean,
-			isRadio?: boolean,
-		} = {
+		isRequired = false,
+		isSrOnly = false,
+		isRadio = false,
+	}: {
+		isRequired?: boolean,
+		isSrOnly?: boolean,
+		isRadio?: boolean,
+	} = {
 			isRequired: false,
 			isSrOnly: false,
 			isRadio: false,
@@ -128,5 +130,110 @@ export class NgRenderer {
 		$inputGroup.appendChild($input);
 
 		return $inputGroup;
+	}
+
+	/**
+	 * Renderer2 implementations
+	 */
+
+	// tslint:disable-next-line: member-ordering
+	public data: { [key: string]: any } = Object.create(null);
+
+	// tslint:disable-next-line: member-ordering
+	public destroyNode: null;
+
+	public destroy() {
+		return;
+	}
+
+	public createElement<T extends keyof HTMLElementTagNameMap>(tagName: T): HTMLElementTagNameMap[T];
+	public createElement(tagName: string): HTMLUnknownElement;
+	public createElement<T extends keyof HTMLElementTagNameMap>(tagName: T) {
+		return this.document.createElement(tagName);
+	}
+
+	public createText(value: string) {
+		return this.document.createTextNode(value);
+	}
+
+	public createComment(value: string) {
+		return this.document.createComment(value);
+	}
+
+	public appendChild(parent: HTMLElement, newChild: HTMLElement) {
+		parent.appendChild(newChild);
+	}
+
+	public removeChild(parent: HTMLElement, newChild: HTMLElement) {
+		parent.removeChild(newChild);
+	}
+
+	public insertBefore(parent: HTMLElement, newChild: HTMLElement, refChild: HTMLElement) {
+		parent.insertBefore(newChild, refChild);
+	}
+
+	public addClass<T extends HTMLElement>(el: T, name: string) {
+		el.classList.add(name);
+	}
+
+	public removeClass<T extends HTMLElement>(el: T, name: string) {
+		el.classList.remove(name);
+	}
+
+	public setStyle<T extends HTMLElement>(el: T, style: string, value: any, important?: boolean) {
+		el.style.setProperty(style, value, important ? 'important' : '');
+	}
+
+	public removeStyle<T extends HTMLElement>(el: T, style: string) {
+		el.style.removeProperty(style);
+	}
+
+	public setAttribute<T extends HTMLElement>(el: T, name: string, value: string) {
+		el.setAttribute(name, value);
+	}
+
+	public removeAttribute<T extends HTMLElement>(el: T, name: string) {
+		el.removeAttribute(name);
+	}
+
+	public setProperty<T extends HTMLElement, U extends Exclude<WritableKeysOf<T>, number | symbol>>(el: T, name: U, value: any) {
+		el[name] = value;
+	}
+
+	public setValue<T extends HTMLElement>(node: T, value: string) {
+		node.nodeValue = value;
+	}
+
+	public parentNode(node: HTMLElement) {
+		return node.parentNode;
+	}
+
+	public nextSibling(node: HTMLElement) {
+		return node.nextSibling;
+	}
+
+	public selectRootElement(selectorOrNode: string | HTMLElement, preserveContent = false): HTMLElement {
+		const el: HTMLElement | null = typeof selectorOrNode === 'string'
+			? document.querySelector(selectorOrNode)
+			: selectorOrNode;
+
+		if (el == null) {
+			throw new Error(`The selector "${selectorOrNode}" did not match any elements`);
+		}
+
+		if (preserveContent === false) {
+			el.textContent = '';
+		}
+
+		return el;
+	}
+
+	public listen<T extends keyof GlobalEventHandlersEventMap>(
+		target: HTMLElement,
+		eventName: T,
+		callback: (event: GlobalEventHandlersEventMap[T]) => boolean | undefined,
+	) {
+		target.addEventListener(eventName, callback);
+		return () => target.removeEventListener(eventName, callback);
 	}
 }
