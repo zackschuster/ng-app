f860a46d0d9fc2ac0265f8d770fbc8134339fafd
Zack Schuster <email@zackschuster.com>
2019-02-17 20:21:41 -0800

    test: enable requestAnimationFrame

--- test/-register.js
+++ test/-register.js
@@ -1,5 +1,5 @@
 require('ts-node').register({ transpileOnly: true, ignore: [] });
-require('browser-env')({ resources: 'usable' });
+require('browser-env')({ pretendToBeVisual: true });
 // @ts-ignore
 require('angular/angular.js');
 require('angular-mocks');
@@ -8,19 +8,22 @@ const defineGlobal = /**
  * @param {string} name
  * @param {any} value
  */
- (name, value) => {
-	Object.defineProperty(global, name, {
-		configurable: false,
-		enumerable: true,
-		writable: false,
-		value
-	});
-}
+	(name, value) => {
+		Object.defineProperty(global, name, {
+			configurable: false,
+			enumerable: true,
+			writable: false,
+			value
+		});
+	}
 
+const angular = /** @type {Window & { angular: import('angular') }} */(window).angular;
 const fetch = require('node-fetch');
+const { Request, Response } = fetch;
+
+defineGlobal('angular', angular);
 defineGlobal('fetch', fetch);
-defineGlobal('Request', fetch.Request);
-defineGlobal('Response', fetch.Response);
-defineGlobal('angular', /** @type {Window & { angular: import('angular') }} */(window).angular);
+defineGlobal('Request', Request);
+defineGlobal('Response', Response);
 
 require('@uirouter/angularjs');
