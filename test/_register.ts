d445ce5389674796368bafe68e96815e3a6b7c12
Zack Schuster <email@zackschuster.com>
2020-07-31 05:42:21 -0700

    chore: update test config

--- test/_register.ts
+++ test/_register.ts
@@ -1,8 +1,6 @@
-// tslint:disable:file-name-casing no-magic-numbers
-require('esm');
+// tslint:disable:file-name-casing no-magic-numbers no-var-requires
 require('ts-node').register({ transpileOnly: true, ignore: [] });
 require('browser-env')({ resources: 'usable' });
-// @ts-ignore
 require('angular/angular.js');
 require('angular-mocks');
 
@@ -10,7 +8,7 @@ Object.defineProperty(global, 'angular', {
 	configurable: false,
 	enumerable: true,
 	writable: false,
-	value: /** @type {*} */(window).angular,
+	value: window.angular,
 });
 
 // taken from https://github.com/PeculiarVentures/node-webcrypto-ossl/blob/6ca13d14243901bb52c195ee0fee70bc4e58fd84/lib/webcrypto.ts#L39
@@ -19,12 +17,13 @@ Object.defineProperty(global, 'crypto', {
 	enumerable: true,
 	writable: false,
 	value: {
+		// @ts-expect-error
 		getRandomValues(array) {
 			if (array) {
 				if (array.byteLength > 65536) {
 					const error = new Error(`ERR_RANDOM_VALUE_LENGTH ${array.byteLength}`);
-					/** @type {*} */
-					(error).code = 22;
+					// @ts-expect-error
+					error.code = 22;
 					throw error;
 				}
 				const bytes = require('crypto').randomBytes(array.byteLength);
