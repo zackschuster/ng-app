bec582c1fc8d8d595eabe42a913b741097caa2f6
Zack Schuster <email@zackschuster.com>
2020-11-04 01:21:08 -0800

    chore: update polyfills

--- docs/src/polyfills.ts
+++ docs/src/polyfills.ts
@@ -1,6 +1,4 @@
 // tslint:disable:no-var-keyword prefer-const no-this-assignment
 import 'whatwg-fetch';
 import 'abortcontroller-polyfill/dist/polyfill-patch-fetch';
-import 'core-js/es/map';
 import 'core-js/es/promise';
-import 'core-js/es/set';
