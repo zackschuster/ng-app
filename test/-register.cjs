e44b64bec2558b4bb34cf9c58b109311e0ecd876
Zack Schuster <email@zackschuster.com>
2020-12-09 17:55:14 -0800

    test: define side-effect requires in config

--- test/-register.cjs
+++ test/-register.cjs
@@ -1,12 +1,7 @@
 // tslint:disable:no-var-requires
-Object.defineProperty(globalThis, 'window', { value: require('browser-env')() });
-
-// @ts-expect-error -- default require doesn't assign angular to window
-require('angular/angular.js');
-require('angular-mocks');
 globalThis.angular = window.angular;
 
-// run tests against bundle (intended for prepublisOnly script)
+// run tests against bundle (intended for prepublishOnly script)
 if (process.title === '../build/ng-app.cjs') {
 	require(process.title);
 	require.cache[require.resolve('../index.ts')] = require.cache[require.resolve(process.title)];
