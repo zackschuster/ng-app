f1f42b5204a0feefca65807e973796c974190d6f
Zack Schuster <email@zackschuster.com>
2021-01-20 13:33:01 -0800

    test/controller: add unit

--- test/controller.ts
+++ test/controller.ts
@@ -1,11 +1,16 @@
 import test from 'ava';
 import { $ctrl, $prefix } from './mocks';
+import { NgAttributes } from '../index';
+
+test('$ctrl.$attrs', t => {
+	t.true($ctrl.$attrs instanceof NgAttributes);
+});
 
 test('$ctrl.apiPrefix', t => {
 	t.is($ctrl.apiPrefix, $prefix);
 });
 
-test('$compCtrl.uniqueId', t => {
+test('$ctrl.uniqueId', t => {
 	t.is(typeof $ctrl.uniqueId, 'string');
 	t.is($ctrl.uniqueId.indexOf('-'), -1);
 	t.is($ctrl.uniqueId.indexOf(','), -1);
